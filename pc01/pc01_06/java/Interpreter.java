55b576e87fec4194e43bfca93f27d64bc05bc651
AlbertNewton254 <miguelmochizukisilva@gmail.com>
2025-08-19 17:52:40 -0300

    feat: add Interpreter.java to pc01_06

---
+++ pc01/pc01_06/java/Interpreter.java
@@ -0,0 +1,129 @@
+/* interpreter
+ * pg110106
+ * uva10033 */
+
+import java.util.Scanner;
+
+public class Interpreter {
+	private final int REGISTERS = 10;
+	private final int RAMSIZE = 1000;
+
+	private int[] registers;
+	private String[] ram;
+
+	private int programCounter;
+	private int instructionsExecuted;
+
+	public Interpreter() {
+		this.registers = new int[REGISTERS];
+		this.ram = new String[RAMSIZE];
+
+		this.programCounter = 0;
+		this.instructionsExecuted = 0;
+
+		initialize();
+	}
+
+	public void initialize() {
+		for (int i = 0; i < REGISTERS; i++) {
+			registers[i] = 0;
+		}
+
+		for (int i = 0; i < RAMSIZE; i++) {
+			ram[i] = "000";
+		}
+
+		programCounter = 0;
+		instructionsExecuted = 0;
+	}
+
+	public void loadProgram(Scanner scanner) {
+		int address = 0;
+
+		while (address < RAMSIZE && scanner.hasNextLine()) {
+			String line = scanner.nextLine().trim();
+
+			if (line.isEmpty()) {
+				break;
+			}
+
+			ram[address] = line;
+			address++;
+		}
+	}
+
+	public void executeProgram() {
+		boolean running = true;
+
+		while (running && programCounter < RAMSIZE) {
+			String instruction = ram[programCounter];
+
+			int opcode = Integer.parseInt(instruction.substring(0,1));
+			int d = Integer.parseInt(instruction.substring(1, 2));
+			int n = Integer.parseInt(instruction.substring(2, 3));
+
+			instructionsExecuted++;
+
+			switch (opcode) {
+				case 0:
+					if (registers[n] != 0) {
+						programCounter = registers[d];
+					} else {
+						programCounter++;
+					}
+					break;
+
+				case 1:
+					if (d == 0 && n == 0) {
+						running = false;
+					} else {
+						programCounter++;
+					}
+					break;
+
+				case 2:
+					registers[d] = n;
+					programCounter++;
+					break;
+
+				case 3:
+					registers[d] = (registers[d] + n) % 1000;
+					programCounter++;
+					break;
+
+				case 4:
+					registers[d] = (registers[d] * n) % 1000;
+					programCounter++;
+					break;
+
+				case 5:
+					registers[d] = registers[n];
+					programCounter++;
+					break;
+
+				case 6:
+					registers[d] = (registers[d] + registers[n]) % 1000;
+					programCounter++;
+					break;
+
+				case 7:
+					registers[d] = (registers[d] * registers[n]) % 1000;
+					programCounter++;
+					break;
+
+				case 8:
+					registers[d] = Integer.parseInt(ram[registers[n]]);
+					programCounter++;
+					break;
+
+				case 9:
+					ram[registers[n]] = String.format("%03d", registers[d]);
+					programCounter++;
+					break;
+
+				default:
+					programCounter++;
+			}
+		}
+	}
+}
