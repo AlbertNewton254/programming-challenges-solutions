7d0d6727a92251a131b580a76738d51c93a8ed68
AlbertNewton254 <miguelmochizukisilva@gmail.com>
2025-08-20 10:13:32 -0300

    feat: update pc01_08.py

--- pc01/pc01_08/python/pc01_08.py
+++ pc01/pc01_08/python/pc01_08.py
@@ -3,3 +3,30 @@ australian voting
 pc110108
 uva10142
 """
+
+import sys
+
+def main():
+    data = sys.stdin.read().splitlines()
+    t = int(data[0].strip())
+    index = 1
+
+    for _ in range(t):
+        while index < len(data) and data[index].strip() == '':
+            index += 1
+
+        number_of_candidates = int(data[index].strip())
+        index += 1
+
+        candidates = []
+        for i in range(number_of_candidates):
+            candidates.append(data[index].strip())
+            index += 1
+
+        votes = []
+        while index < len(data) and data[index].strip() != '':
+            preferences = list(map(int, data[index].split()))
+            votes.append(preferences)
+            index += 1
+
+        eliminated = [False] * number_of_candidates
