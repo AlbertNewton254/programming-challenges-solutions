e41a4b05380f255cbe24f7da538a5bb44afd3dc7
AlbertNewton254 <miguelmochizukisilva@gmail.com>
2025-08-21 01:39:38 -0300

    test: add time of execution output to pc01_01.c

--- pc01/pc01_01/c/pc01_01.c
+++ pc01/pc01_01/c/pc01_01.c
@@ -3,6 +3,7 @@
  * uva100 */
 
 #include <stdio.h>
+#include <time.h>
 
 const int MAX_NUMBER = (int) 1E6;
 
@@ -53,6 +54,11 @@ int max_collatz_length(int number1, int number2) {
 }
 
 int main(void) {
+	clock_t start, end;
+	double cpu_time_used;
+
+	start = clock();
+
 	int number1, number2;
 
 	while (1) {
@@ -79,6 +85,11 @@ int main(void) {
 			return -1;
 		}
 	}
+
+	end = clock();
+
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Time of execution: %lf seconds\n", cpu_time_used);
 
 	return 0;
 }
